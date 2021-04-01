b58c08b28612a94d049164ed33a8e3c419d63eee
aKn8194 <knguyenanh8194@gmail.com>
2021-04-02 00:09:56 +0700

    Update Exercises

--- Exercises/Bai_11.cs
+++ Exercises/Bai_11.cs
@@ -1,4 +1,9 @@
-//Bài 1: Tính S(n) = 1 + 2 + 3 + … + n
+// Bài 11: Tính S(n) = 1 + 1.2 + 1.2.3 + … +1.2.3….N
+/*
+S(1) = 1!
+S(2) = 1! + 2!
+S(3) = 1! + 2! + 3!
+ */
 
 using System;
 
@@ -6,8 +11,35 @@ namespace Bai_11
 {
     class Program
     {
+        static int TinhGiaiThua(int n)
+        {
+            int kq = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                kq = kq * i;
+            }
+            return kq;
+
+        }
+
+        static int TinhTong(int n)
+        {
+            int kq = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                kq = kq + TinhGiaiThua(i);
+            }
+            return kq;
+        }
         static void Main(string[] args)
         {
+            int N, a;
+            Console.WriteLine("Nhap N: ");
+            N = int.Parse(Console.ReadLine());
+            a = TinhTong(N);
+            Console.WriteLine($"Ket qua cua s la {a}");
+
+            /* --- Cach cu
             int N;
             Console.WriteLine("Nhap N: ");
             N = int.Parse(Console.ReadLine());
@@ -19,17 +51,17 @@ namespace Bai_11
 
             while (i <= N)
             {
-                
+
                 while (t <= i)
                 {
-                    u = u * t; 
-                    t = t + 1; 
+                    u = u * t;
+                    t = t + 1;
                 }
                 s = s + u;
-                i = i+1;
+                i = i + 1;
             }
             Console.WriteLine($"Ket qua cua s la {s}");
+            */
         }
     }
-
 }
