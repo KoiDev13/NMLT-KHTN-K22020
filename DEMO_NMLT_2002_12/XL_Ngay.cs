1adcaeb7dad783df10d543309851388e332f56dd
aKn8194 <knguyena8194@gmail.com>
2021-04-26 08:57:17 +0700

    Update demo cua giao vien

---
+++ DEMO_NMLT_2002_12/XL_Ngay.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DEMO_NMLT_2002_12
+{
+    class XL_Ngay
+    {
+        public static int TimSoNgayTrongThang(int thang, int nam)
+        {
+            int ngayTrongThang = 31;
+            if (thang == 4 || thang == 6 || thang == 9 || thang == 11)
+            {
+                ngayTrongThang = 30;
+            }
+            if (thang == 2)
+            {
+                if ((nam % 4 == 0 && nam % 100 != 0) || (nam % 400 == 0))
+                {
+                    ngayTrongThang = 29;
+                }
+                else
+                {
+                    ngayTrongThang = 28;
+                }
+            }
+
+            return ngayTrongThang;
+        }
+
+        public static void TimNgayKeTiep(int ngay, int thang, int nam, out int ngayke, out int thangke, out int namke)
+        {
+            int ngayTrongThang = TimSoNgayTrongThang(thang, nam);
+
+            ngayke = ngay + 1;
+            thangke = thang;
+            namke = nam;
+
+            if (ngayke > ngayTrongThang)
+            {
+                ngayke = 1;
+                thangke = thangke + 1;
+            }
+
+            if (thangke > 12)
+            {
+                thangke = 1;
+                namke++;
+            }
+        }
+    }
+}
---
+++ DEMO_NMLT_2002_12/XL_SONGUYEN.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DEMO_NMLT_2002_12
+{
+    class XL_SONGUYEN
+    {
+        public static void HoanVi(ref int x, int y)
+        {
+            int t = x;
+            x = y;
+            y = t;
+        }
+        public static int TinhGiaiThua(int n)
+        {
+            int kq = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                kq = kq * i;
+            }
+            return kq;
+        }
+        public static bool KiemTraNguyenTo(int n)
+        {
+            if (n <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public static void LietKeNguyenTo(int n)
+        {
+            for (int i = 2; i <= n; i++)
+            {
+                if (KiemTraNguyenTo(i) == true)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        public static int TinhS11(int n)
+        {
+            int kq = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                kq = kq + TinhGiaiThua(i);
+            }
+            return kq;
+        }
+        public static int TimMax(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+
+            return b;
+        }
+    }
+}
