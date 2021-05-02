04c9f3d25d3989ed9467c2cf1ba183c8c70b9c1a
aKn8194 <knguyenanh8194@gmail.com>
2021-05-02 13:59:37 +0700

    Update class XL_MATRAN

--- NMLT_FULL/XL_MATRAN.cs
+++ NMLT_FULL/XL_MATRAN.cs
@@ -6,6 +6,7 @@ namespace Test_NMLT
 {
     class XL_MATRAN
     {
+        //Bài 311 + 313: Viết hàm nhập xuất ma trận số nguyên
         public static int[,] NhapMaTran()
         {
             int[,] A;
@@ -20,7 +21,7 @@ namespace Test_NMLT
                 for (int j = 0; j < M; j++)
                 {
                     Console.Write($"A[{i},{j}] = ");
-                    A[i,j] = int.Parse(Console.ReadLine());
+                    A[i, j] = int.Parse(Console.ReadLine());
                 }
             }
             return A;
@@ -51,6 +52,8 @@ namespace Test_NMLT
             }
             return S;
         }
+
+        //Bài 315: Viết hàm tìm giá trị lớn nhất trong ma trận số thực
         public static int TimMax(int[,] A)
         {
             int Max = A[0, 0];
@@ -67,5 +70,38 @@ namespace Test_NMLT
             return Max;
         }
         
+        //Bài 317: Viết hàm đếm số lượng số nguyên tố trong ma trận số nguyên
+        public static int DemNguyenTo(int[,] A)
+        {
+            int Dem = 0;
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+                    if (XL_SONGUYEN.KiemTraNguyenTo(A[i, j]) == true)
+                    {
+                        Dem++;
+                    }
+                }
+            }
+            return Dem;
+        }
+
+        //Bài 327: Tính tổng các giá trị nằm trên biên của ma trận
+        public static int TongBien(int[,] A)
+        {
+            int s = 0;
+            for (int j = 0; j < A.GetLength(1); j++) // Tinh 2 dong bien
+            {
+                s = s + A[0, j];
+                s = s + A[A.GetLength(0)-1, j]; // Dong cuoi 
+            }
+            for (int i = 1; i < A.GetLength(0)-1; i++) // Tinh 2 cot bien , nho bo dong dau tien
+            {
+                s = s + A[i,0];
+                s = s + A[i, A.GetLength(1)-1]; // Cot cuoi
+            }
+            return s;
+        }
     }
 }
